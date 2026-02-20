3bd606ae2642cfb2eeb8f4d7a2ef0faaf5577243
Shehryar Ahmad <84899690+sheri-dhillon@users.noreply.github.com>
2026-02-20 23:42:43 +0500

    package file fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Handle SPA routing - redirect all requests to index.html
-    historyApiFallback: true,
-  },
 })
